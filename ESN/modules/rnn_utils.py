de8151735cd90e493576cc6e8285fe02d3e1b0cc
tkc-morita <tmorita@alum.mit.edu>
2020-12-01 21:19:08 +0900

    Made _VF._pad_packed_sequence compatible with PyTorch>=1.5

--- ESN/modules/rnn_utils.py
+++ ESN/modules/rnn_utils.py
@@ -1,6 +1,11 @@
 # coding: utf-8
 
-import torch
+try:
+	# for pytorch<=1.4
+	from torch.nn._VF import _pad_packed_sequence
+except:
+	# for pytorch>=1.5
+	from torch._VF import _pad_packed_sequence
 
 def pad_flatten_sequence(flatten, batch_sizes, padding_value=0.0, batch_first=False):
 	return torch.nn._VF._pad_packed_sequence(
